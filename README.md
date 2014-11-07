a87f6cb66f5888b37eef2ed27ccf84669ce6141d
Filip Malczak <filip.malczak@gmail.com>
2014-11-07 22:55:13 +0100

    Initial and probably final commit

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-addic7ed-checker
+tv-checker
 ================
 
 Simple python script for checking whether you favourite TV series has any new episodes.
+
+This script has 2 modes:
+
+* Checking followed series for new episodes (tv_checker.py check ...)
+* Adding new series to followed (tv_checker.py add ...)
+
+Use flag -h/--help to see full usage.
+
+Script assumes you use it properly. It's UI is really simple, so there are no real error handlers - if something goes wrong, you'll
+see fugly stacktrace, but I think you'll figure it out.
+
+"New" episode means "episode that isn't one that is saved in config". I wrote this, because I couldn't really remember when do new 
+episodes come out, so instead of checking manually I automatized it. It won't be really useful, when you want to watch some series 
+from the very beginning.
+
+Frankly, this should be GIST, not full-blown repo, but I wanted to provide example configuration (tv_config.json) too.
+
+This is too simple to provide real setup.py. Here are its dependencies (exact version I used are shown too, but it will work with most
+versions of those packages):
+
+* requests (2.4.3)
+* bs4 (4.3.2)
+* docopt (0.6.2)
+
+Whole tool is licensed as MIT-ish beerware.
+
---
+++ tv_checker.py
@@ -0,0 +1,78 @@
+#!/bin/env python3
+
+import sys
+from bs4 import BeautifulSoup
+import requests
+import json
+from docopt import docopt
+
+USAGE = """Addic7ed TV series subtitles checker
+
+Usage:
+  tv_checker.py check [--config=<config>] [--no-update]
+  tv_checker.py add --name=<name> --base=<base> [--config <config>]
+
+Options:
+  --config <config>             Config file in JSON format. Contained object should have attributes named as 
+                                    followed TV series. Their values should be objects with attribute "base_address", 
+                                    which is URL of episodes list and air dates on Addic7ed, and "last_path" with
+                                    URL path of last episode watched by you (for example, if you watched episode with 
+                                    subtitles that can be found at "http://addic7ed.com/blah/bla/bluh", this should 
+                                    be "/blah/bla/bluh") [default: ./tv_config.json]
+  -n, --no-update               Use this flag if you only want to check whether there are any new episodes, and not 
+                                    download subtitles and watch those episodes now. Without this flag this script will 
+                                    assume that just after running it, you'll watch episodes that it found as new.
+  --name=<name>                 Name of TV series that you want to start to follow. Technically, it doesn't really
+                                    matter, it is used only while notifying you.
+  --base=<base>                 Base URL of added series. See --config explanation for details.
+  -h, --help                    Show this.
+  -v, --version                 Show version of this script. WARNING: That will not be very helpful."""
+  
+VERSION = "one and only"
+
+def last_link(base_address):
+    response = requests.get(base_address)
+    assert response.status_code
+    content = response.text
+    soup = BeautifulSoup(content)
+    episode_links = soup.find_all("tr", "hoverme")
+    return episode_links[-1].find_all("td")[-1].a["href"]
+
+def notify(name, path):
+    print('New subtitles for "'+name+'" found:')
+    print("\thttp://addic7ed.com"+path)
+
+def check(update_config, config):
+    for name, descriptor in config.items():
+        
+        last = last_link(descriptor["base_address"])
+        
+        if not last==descriptor["last_path"]:
+            notify(name, last)
+            if update_config:
+                descriptor["last_path"] = last
+
+def add(name, base, config):
+    config[name] = {
+            "base_address": base,
+            "last_path": last_link(base)
+        }
+
+def main(args):
+    parsed = docopt(USAGE, args, True, VERSION)
+    
+    with open(parsed["--config"], "r") as f:
+        config = json.load(f)
+    
+    if parsed["check"]:
+        check(not parsed["--no-update"], config)
+        if not parsed["--no-update"]:
+            with open(parsed["--config"], "w") as f:
+                json.dump(config, f, indent=4)
+    elif parsed["add"]:
+        add(parsed["--name"], parsed["--base"], config)
+        with open(parsed["--config"], "w") as f:
+            json.dump(config, f, indent=4)
+        
+if __name__=="__main__":
+    main(sys.argv[1:])
